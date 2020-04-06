dee0f72b37a3b29b3fd1f1c764d23584a6f82852
OSLong <osunlong@gmail.com>
2020-04-07 03:54:20 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,9 +1,15 @@
+
+# Input process name for search
 $Select = Read-Host "Input Process Name"
 
+# List All Process
 tasklist | Select-String $Select
 
+# Process ID
 $id = Read-Host  -Prompt "Input PID : "
 
 
-Write-Host 
-agth /P$id /C
+Write-Host ""
+
+# Excute agth As Admin
+powershell -Command "Start-Process powershell  'agth /P$id /C /KF' -Verb RunAs"
